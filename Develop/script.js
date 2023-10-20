916d49bd04d847d98aa4480ddf8e8caedfbf27dd
Sam Brez <sambreslin02@gmail.com>
2023-10-20 17:37:23 -0400

    initial prompt  input validation

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 const generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  let length = prompt("How many characters do you want the password to contain?");
+  const length = prompt("How many characters do you want the password to contain?");
   if (length < 8) {
     alert("Not enough characters.")
     return
@@ -11,24 +11,17 @@ function generatePassword() {
     alert("Too many characters.")
     return
   }
-  let lower = confirm("Click OK to include lowercase letters.");
-  let upper = confirm("Click OK to include uppercase letters.");
-  let numbers = confirm("Click OK to include numbers.");
-  let special = confirm("Click OK to include special characters.");
-  // let charaterType = {
-  //   lowerCaseLetter: "abcdefghijklmnopqrstuvwxyz",
-  //   upperCaseLetter: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  //   numbers: "0123456789",
-  //   special: " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-  //   }
   
-    return;
+  const lower = confirm("Click OK to include lowercase letters.");
+  const upper = confirm("Click OK to include uppercase letters.");
+  const numbers = confirm("Click OK to include numbers.");
+  const special = confirm("Click OK to include special characters.");
   };
 
 
 // writePassword function contains undefined functions (generatePassword)
 function writePassword() {
-  const password = generatePassword();
+  let password = generatePassword();
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -37,3 +30,25 @@ function writePassword() {
 
 // when "generate password" button is clicked, writePassword function is called
 generateBtn.addEventListener("click", writePassword);
+
+
+// let characterSet = {
+//   lowerCaseLetter: "abcdefghijklmnopqrstuvwxyz",
+//   upperCaseLetter: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//   numbers: "0123456789",
+//   special: " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+//   }
+
+
+// const charTypes = {
+    
+// };
+// if (charTypes.length === 0) {
+//   alert("No character types selected.")
+//   return
+// }
+
+// let password = '';
+//   for (let i = 0; i < length; i++) {
+
+//   }
