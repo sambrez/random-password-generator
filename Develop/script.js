f9fa206149f302a15c52e11e64b62cb4cad6bd8b
Sam Brez <sambreslin02@gmail.com>
2023-10-20 16:15:08 -0400

    addition of prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,30 @@
 // targeting the generate password button
 const generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  let length = prompt("How many characters do you want the password to contain?");
+  if (length < 8) {
+    alert("Not enough characters.")
+    return
+  }
+  if (length > 128) {
+    alert("Too many characters.")
+    return
+  }
+  let lower = confirm("Click OK to include lowercase letters.");
+  let upper = confirm("Click OK to include uppercase letters.");
+  let numbers = confirm("Click OK to include numbers.");
+  let special = confirm("Click OK to include special characters.");
+  // let charaterType = {
+  //   lowerCaseLetter: "abcdefghijklmnopqrstuvwxyz",
+  //   upperCaseLetter: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  //   numbers: "0123456789",
+  //   special: " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  //   }
+  
+    return;
+  };
+
 
 // writePassword function contains undefined functions (generatePassword)
 function writePassword() {
