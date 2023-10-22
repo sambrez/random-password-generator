fdb3a661f22133a547ca3fe75037fc7a4e227737
Sam Brez <sambreslin02@gmail.com>
2023-10-22 09:11:55 -0400

    length and characterset variables finalized

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ let characterSet = {
   }
 
 function generatePassword() {
-  // variable prompts the user and the if statment validates the user input
+  // variable prompts the user on password length and the if statment validates the user input
   let length = prompt("How many characters do you want the password to contain?");
   if (length < 8) {
     alert("Not enough characters.")
@@ -20,34 +20,35 @@ function generatePassword() {
     return
   }
   
-  // the variables prompt the user and the if statement validates the user input
+  // the variables prompt the user on character type and the if statement validates the user input
   const lower = confirm("Click OK to include lowercase letters.");
   const upper = confirm("Click OK to include uppercase letters.");
   const numbers = confirm("Click OK to include numbers.");
   const special = confirm("Click OK to include special characters.");
+  let charString = "";
+  let finalPassword = "";
   if (lower || upper || numbers || special) {}
   else {
     alert("No characters were selected.")
     return
   }
 
-  let charString = "";
   if (lower) {
-   charString = characterSet.lowerCaseLetter
+   charString = charString + characterSet.lowerCaseLetter
   };
   if (upper) {
-    charString = characterSet.upperCaseLetter
+    charString = charString + characterSet.upperCaseLetter
   };
   if (numbers) {
-    charString = characterSet.digits
+    charString = charString + characterSet.digits
   };
   if (special) {
-    charString = characterSet.symbols
+    charString = charString + characterSet.symbols
   };
- 
 
+for ()
 
- return
+ return finalPassword;
 
   };
 
